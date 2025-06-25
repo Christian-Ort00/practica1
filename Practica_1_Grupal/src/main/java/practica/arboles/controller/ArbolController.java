6f31e5d5740f714827ef0e64c0c72d02e944c0e1
LeonardoFioravanti <160791947+LeonardoFioravanti@users.noreply.github.com>
2025-06-25 01:04:25 -0600

    Commit
    corrección

--- Practica_1_Grupal/src/main/java/practica/arboles/controller/ArbolController.java
+++ Practica_1_Grupal/src/main/java/practica/arboles/controller/ArbolController.java
@@ -16,24 +16,24 @@ public class ArbolController {
     @Autowired
     private ArbolService arbolService;
 
-    // Mostrar listado de árboles
+
     @GetMapping("/listado")
     public String inicio(Model model) {
         var arboles = arbolService.getArboles();
         model.addAttribute("arboles", arboles);
         model.addAttribute("totalArboles", arboles.size());
-        model.addAttribute("arbol", new Arbol()); // Necesario para el formulario
+        model.addAttribute("arbol", new Arbol()); 
         return "/arbol/listado";
     }
 
-    // Eliminar árbol
+
     @GetMapping("/eliminar/{id}")
     public String eliminaArbol(Arbol arbol) {
         arbolService.delete(arbol);
         return "redirect:/arbol/listado";
     }
 
-    // Cargar árbol para edición
+
     @GetMapping("/modificar/{id}")
     public String modificaArbol(Arbol arbol, Model model) {
         arbol = arbolService.getArbol(arbol);
@@ -41,7 +41,7 @@ public class ArbolController {
         return "/arbol/modifica";
     }
 
-    // Guardar árbol (nuevo o modificado)
+
     @PostMapping("/guardar")
     public String guardaArbol(Arbol arbol) {
         arbolService.save(arbol);
