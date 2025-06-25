5ae84bf6810410df7c490e8ca35eb221ee514feb
Christian-Ort00 <cortega20867@ufide.ac.cr>
2025-06-24 21:55:48 -0600

    1 commit

---
+++ Practica_1_Grupal/src/main/java/practica/arboles/ArbolesApplication.java
@@ -0,0 +1,13 @@
+package practica.arboles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbolesApplication.class, args);
+	}
+
+}
---
+++ Practica_1_Grupal/src/test/java/practica/arboles/ArbolesApplicationTests.java
@@ -0,0 +1,13 @@
+package practica.arboles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
