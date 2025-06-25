fd85ee9f7ce080bb6ea0ec580c3a1e23435b8458
Wainer22 <wainercr22@gmail.com>
2025-06-25 01:16:00 -0600

    Página principal

---
+++ Practica_1_Grupal/src/main/java/practica/arboles/controller/InicioController.java
@@ -0,0 +1,12 @@
+//package practica.arboles.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class InicioController {
+//    @GetMapping("/")
+//    public String inicio() {
+//        return "index"; // Debes tener una vista llamada index.html en templates
+//    }
+//}
